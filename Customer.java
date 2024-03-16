3a45b1073cbaf57fc44319a530c4d58e6dd24efd
M. Onur Aysel <93475143+LyotardPostmodernizm@users.noreply.github.com>
2024-03-17 01:04:49 +0300

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Customer {
+    // write code here
+    private String name;
+    private ArrayList<Double>transactions;
+    
+    public Customer(String name, double initialTransaction){
+        this.name = name;
+        transactions =new ArrayList<Double>();
+        transactions.add(initialTransaction);
+        
+    }
+    public String getName(){
+        return name;
+    }
+    public ArrayList<Double> getTransactions(){
+        return transactions;
+    }
+    public void addTransaction(double transaction){
+        transactions.add(transaction);
+    }
+    
+    
+    
+}
